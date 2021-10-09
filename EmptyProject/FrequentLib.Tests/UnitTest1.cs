3ebda84bc790e1357404d07b3861029f0a16b8b1
Fathi <chbnfathi@hotmail.com>
2021-10-09 23:24:02 +0200

    Second try

--- EmptyProject/FrequentLib.Tests/UnitTest1.cs
+++ EmptyProject/FrequentLib.Tests/UnitTest1.cs
@@ -11,5 +11,12 @@ namespace FrequentLib.Tests
 
             Assert.AreEqual(1, 1);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            Assert.AreEqual(1, 2);
+        }
     }
 }
